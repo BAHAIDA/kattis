33208373e6dcecbdb355c7d3697531b343283e8c
Aabir <abahaida@pca.ma>
2018-12-06 08:44:08 +0100

    init repo

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line))
+            {
+                decimal sum = 0;
+                decimal prod = 1;
+                Dictionary<char, decimal> WordCount = new Dictionary<char, decimal>();
+                foreach(char c in line)
+                {
+                    if (WordCount.ContainsKey(c))
+                        WordCount[c]++;
+                    else
+                        WordCount.Add(c, 1);
+                }
+                foreach(var elt in WordCount)
+                {
+                    sum += elt.Value;
+                    prod = prod * factoriel(elt.Value);
+                }
+                Console.WriteLine(String.Format("{0:F0}", factoriel(sum) * prod));
+                line = Console.ReadLine();
+
+            }
+        }
+        static decimal factoriel(decimal param)
+        {
+            if (param == 1)
+                return 1;
+            else return param * factoriel(param - 1);
+        }
+        static decimal ReverseFactoriel(decimal param)
+        {
+            if (param == 1)
+                return 1;
+            else return 1/param * ReverseFactoriel(param - 1);
+        }
+    }
+}
